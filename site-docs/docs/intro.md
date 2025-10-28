3ff539d6640975ad52f2b201ce8073283ae9e98d
Massari33 <massarigabriel73@gmail.com>
2025-10-28 09:18:41 -0300

    Alteracao-docs
    implementando algumas funcionalidades e inforacoes

--- site-docs/docs/intro.md
+++ site-docs/docs/intro.md
@@ -4,5 +4,5 @@ sidebar_position: 1
 
 #  Projeto Integrado Multidisciplinar
 
-1
+Por aqui você encontrará nossos projetos com informações técnicas e intruções de uso
 
--- site-docs/src/pages/index.js
+++ site-docs/src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-             Ir para o Sistema
+             Ir para a Documentação
           </Link>
         </div>
       </div>
