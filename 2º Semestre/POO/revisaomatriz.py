ec3e684f0028c9a47a544e77b87407fa9cf8dd46
ramongui0501 <ramongui0501@gmail.com>
2026-03-23 21:28:58 -0300

    Aula rogerio revisão

---
+++ 2º Semestre/POO/revisaomatriz.py
@@ -0,0 +1,37 @@
+
+class pessoa:
+
+    def __init__(self, nome):
+        self.nome = nome
+        self.idade = idade
+class idade:
+
+    def __init__(self, idade):
+        self.idade = idade
+
+pessoa1 = pessoa("Ramon")
+idade1 = idade ("20")
+
+print(pessoa1.nome)
+print(idade1.idade)
+
+pessoa2 = pessoa("Gabriel")
+idade2 = idade ("21")
+
+print(pessoa2.nome)
+print(idade2.idade)
+
+pessoa3 = pessoa("Peu")
+idade3 = idade ("19")
+
+print(pessoa3.nome)
+print(idade3.idade)
+
+pessoa4 = pessoa("Jean")
+idade4 = idade ("21")
+
+print(pessoa4.nome)
+print(idade4.idade)
+
+
+
